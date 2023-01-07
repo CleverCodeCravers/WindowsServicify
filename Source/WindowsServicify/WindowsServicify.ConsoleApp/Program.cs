76a26ededfa0104a5419259b05ff148dfe65ebfc
Stefan Hoffmann <stho32github@gmail.com>
2023-01-07 16:50:41 +0100

    Changed the directory where Servicify will put and get the config from to the exe-Directory

--- Source/WindowsServicify/WindowsServicify.ConsoleApp/Program.cs
+++ Source/WindowsServicify/WindowsServicify.ConsoleApp/Program.cs
@@ -9,6 +9,7 @@ if (!commandLineParametersResult.IsSuccess)
     Console.WriteLine("Unfortunately there have been problems with the command line arguments.");
     return;
 }
+var configurationFilePath = Path.Combine(ExecutablePathHelper.GetExecutablePath(), "config.json");
 
 var parameters = commandLineParametersResult.Value;
 
@@ -16,5 +17,5 @@ if (parameters.Configure)
 {
     Console.WriteLine("Hey There, Let's Configure Windows Servicify Together!");
     var configData = ServiceConfigurationRequester.GetServiceConfiguration();
-    ServiceConfigurationFileHandler.Save(Path.Combine(Directory.GetCurrentDirectory(), "config.json"),configData);
+    ServiceConfigurationFileHandler.Save(configurationFilePath,configData);
 }
---
+++ Source/WindowsServicify/WindowsServicify.Domain/ExecutablePathHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace WindowsServicify.Domain;
+
+public class ExecutablePathHelper
+{
+    public static string GetExecutablePath()
+    {
+        string exeLocation = Assembly.GetExecutingAssembly().Location;
+        string exeDirectory = Path.GetDirectoryName(exeLocation)!;
+        return exeDirectory;
+    }
+}
