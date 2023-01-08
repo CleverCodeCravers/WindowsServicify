2b640c25eb6a42afa02ac4eabdb974a7c5fb8ebc
Stefan Hoffmann <stho32github@gmail.com>
2023-01-08 17:37:31 +0100

    The script now also writes a little log file so we can easily see if it works in service-mode.

--- Documentation/HelloWorld.ps1
+++ Documentation/HelloWorld.ps1
@@ -1,9 +1,14 @@
+Set-Location $PSScriptRoot
+
 $counter = 1
 
 while ($true)
 {
     $counter += 1
     Write-Host "Hello worldalon Nr. $counter"
+
+    "[$(Get-Date)]Hello worldalon Nr. $counter" | Add-Content log.txt
+
     Start-Sleep -Seconds 5
 
     if ($counter -eq 10) {
