7de84420c9b859f6bc53a9645dd07d8d207d9785
naseif <d2mds21@gmail.com>
2023-01-14 20:31:25 +0100

    Added help command

--- Source/WindowsServicify/WindowsServicify.Domain/ConsoleCommandLineParameters.cs
+++ Source/WindowsServicify/WindowsServicify.Domain/ConsoleCommandLineParameters.cs
@@ -1,3 +1,3 @@
 ﻿namespace WindowsServicify.Domain;
 
-public record ConsoleCommandLineParameters(bool Configure, bool Install, bool Uninstall, bool Testrun);
+public record ConsoleCommandLineParameters(bool Configure, bool Install, bool Uninstall, bool Testrun, bool Help);
